7f72947b4d151b748df6b28dfc27bc3fc85c1781
pengfuyuzhen <pfyz2011@gmail.com>
2018-02-05 21:43:18 -0500

    Implemented the chat bot

--- chatServer.js
+++ chatServer.js
@@ -9,6 +9,7 @@ var app = express(); // webapp
 var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
+var numberOfCorrectAnswer = 0;
 
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
@@ -30,13 +31,12 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-  setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
-
+  socket.emit('answer',"Hi there, I am Jarvis, a simple quiz bot!"); //We start with the introduction;
+  setTimeout(timedQuestion, 3000, socket,"What is your name?"); // Wait a moment and respond with a question.
 });
   socket.on('message', (data)=>{ // If we get a new message from the client we process it;
         console.log(data);
-        questionNum= bot(data,socket,questionNum);	// run the bot function with the new message
+        questionNum= bot(data,socket,questionNum);  // run the bot function with the new message
       });
   socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
     console.log('user disconnected');
@@ -51,52 +51,50 @@ function bot(data,socket,questionNum) {
 
 /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
-  waitTime =2000;
-  question = 'How old are you?';			    	// load next question
-  }
-  else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
-  }
-  else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
-  waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
-  }
-  else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
-  socket.emit('changeBG',input.toLowerCase());
-  waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
-  }
-  else if (questionNum == 4) {
-    if(input.toLowerCase()==='yes'|| input===1){
-      answer = 'Perfect!';
-      waitTime =2000;
-      question = 'Whats your favorite place?';
+    answer = 'Hello ' + input + ' :-) The quiz will begin now'; // output response
+    waitTime = 3000;
+    question = 'In what year was Cornell University founded?'; // load next question
+  } else if (questionNum == 1) {
+    if (checkAnswer(input, questionNum)) {
+      answer = 'Correct!';
+      numberOfCorrectAnswer += 1;
+    } else {
+      answer = 'Oops... Cornell was founded in 1865'
     }
-    else if(input.toLowerCase()==='no'|| input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
-        answer=''
-        question='How about now?';
-        waitTime =0;
-        questionNum--; // Here we go back in the question number this can end up in a loop
-    }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
-      question='';
-      questionNum--;
-      waitTime =0;
+    waitTime = 2500;
+    question = 'Which character is the first to speak in Star Wars?'; // load next question
+  } else if (questionNum == 2) {
+    if (checkAnswer(input, questionNum)) {
+      answer = 'Correct!';
+      numberOfCorrectAnswer += 1;
+    } else {
+      answer = "Oops... It's C3PO"
     }
+    waitTime = 2500;
+    question = 'The Statue of Liberty was given to the US by which country?'; // load next question
+  } else if (questionNum == 3) {
+    if (checkAnswer(input, questionNum)) {
+      answer = 'Correct!';
+      numberOfCorrectAnswer += 1;
+    } else {
+      answer = "Oops... It's France"
+    }
+    waitTime = 2500;
+    question = 'The song "My Heart Will Go On" came from what movie?';            // load next question
+  } else if (questionNum == 4) {
+    if (checkAnswer(input, questionNum)) {
+      answer = 'Correct!';
+      numberOfCorrectAnswer += 1;
+    } else {
+      answer = "Oops... It's 'Titanic'"
+    }
+    question = ""
+    waitTime = 2500;
   // load next question
-  }
-  else{
-    answer= 'I have nothing more to say!';// output response
-    waitTime =0;
+  } else {
+    waitTime = 0;
     question = '';
   }
-
 
 /// We take the changed data and distribute it across the required objects.
   socket.emit('answer',answer);
@@ -104,13 +102,27 @@ function bot(data,socket,questionNum) {
   return (questionNum+1);
 }
 
-function timedQuestion(socket,question) {
-  if(question!=''){
-  socket.emit('question',question);
-}
-  else{
-    //console.log('No Question send!');
+function checkAnswer(answer, questionNumber) {
+  var isCorrect = false;
+  if (questionNumber == 1) {
+    if (answer == '1865') isCorrect = true;
+  } else if (questionNumber == 2) {
+    if (answer.toLowerCase() == 'c3po') isCorrect = true;
+  } else if (questionNumber == 3) {
+    if (answer.toLowerCase() == 'france') isCorrect = true;
+  } else if (questionNumber == 4) {
+    if (answer.toLowerCase() == 'titanic') isCorrect = true;
   }
 
+  return isCorrect;
+}
+
+function timedQuestion(socket,question) {
+  if(question!='') {
+    socket.emit('question',question);
+  } else{
+    answer = "Done! You've got " + numberOfCorrectAnswer + "/4 questions correctly"
+    socket.emit('answer',answer);
+  }
 }
 //----------------------------------------------------------------------------//
